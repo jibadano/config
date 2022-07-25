3fb45fd1fac5cd8207ae09058afb9626e376366d
Juan Badano <jb@target-solutions.net>
2022-07-25 13:11:02 +0300

    A

--- init.js
+++ init.js
@@ -144,6 +144,8 @@ const withConfig = async (nextConfig = { env: {} }) => {
   const conf = await initConfig()
   nextConfig.publicRuntimeConfig = nextConfig.publicRuntimeConfig || {}
   nextConfig.publicRuntimeConfig.config = conf
+  nextConfig.env.CONFIG = JSON.stringify(conf)
+
   return nextConfig
 }
 
